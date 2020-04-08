e7ea5179c68a3de0d2972ff7f4d7b9e60aa6c4f0
rohit18115 <rohit18115@iiitd.ac.in>
2020-04-08 17:35:50 +0530

    structured code

---
+++ WaveletCNN/Wavelet/data_io.py
@@ -0,0 +1,141 @@
+import configparser as ConfigParser
+from optparse import OptionParser
+import numpy as np
+import pandas as pd
+
+
+# import scipy.io.wavfile
+
+
+def ReadList(list_file):
+
+    filehandle = open(list_file, 'r')
+
+    protocol = []
+
+    while True:
+
+        # read a single line
+
+        line = (filehandle.readline())
+
+        protocol.append(line)
+
+        if not line:
+
+            break
+
+    # close the pointer to that file
+    filehandle.close()
+
+    protocol = [s[:-1] for s in protocol]
+
+    protocol = pd.DataFrame([s.split(' ') for s in protocol])
+
+    protocol.columns = ['speaker_id', 'file_id', 'blah', 'system_id', 'label']
+
+    protocol = protocol[['speaker_id', 'file_id', 'system_id', 'label']]
+
+    protocol.dropna(inplace=True)
+
+    protocol.drop_duplicates(subset="file_id", keep='first', inplace=True)
+
+    for count, i in enumerate(protocol.iloc[:, -2]):
+        if i == '-':
+            protocol.iloc[count, -2] = 0
+            protocol.iloc[count, -1] = 0
+        elif i == 'A01':
+            protocol.iloc[count, -2] = 1
+            protocol.iloc[count, -1] = 1
+        elif i == 'A02':
+            protocol.iloc[count, -2] = 2
+            protocol.iloc[count, -1] = 1
+        elif i == 'A03':
+            protocol.iloc[count, -2] = 3
+            protocol.iloc[count, -1] = 1
+        elif i == 'A04':
+            protocol.iloc[count, -2] = 4
+            protocol.iloc[count, -1] = 1
+        elif i == 'A05':
+            protocol.iloc[count, -2] = 5
+            protocol.iloc[count, -1] = 1
+        elif i == 'A06':
+            protocol.iloc[count, -2] = 6
+            protocol.iloc[count, -1] = 1
+
+    return protocol
+
+
+def read_conf(cfg_file=None):
+    parser = OptionParser()
+    parser.add_option("--cfg")  # Mandatory
+    (options, args) = parser.parse_args()
+    if cfg_file is None:
+        cfg_file = options.cfg
+    if options.cfg is None and cfg_file is None:
+        cfg_file = r'/media/rohit/New Volume/codes/SA/PycharmProjects/spoof_detection_deep_features/WaveletCNN/Wavelet/cfg/Wavelet_ASV.cfg'
+    Config = ConfigParser.ConfigParser()
+    Config.read(cfg_file)
+
+    # [data]
+    options.train_lst = Config.get('data', 'train_lst')
+    options.dev_lst = Config.get('data', 'dev_lst')
+    # options.lab_dict = Config.get('data', 'lab_dict')
+    options.train_data_folder = Config.get('data', 'train_data_folder')
+    options.dev_data_folder = Config.get('data', 'dev_data_folder')
+    options.output_folder = Config.get('data', 'output_folder')
+    options.pt_file = Config.get('data', 'pt_file')
+
+    # [windowing]
+    options.fs = Config.get('windowing', 'fs')
+    options.cw_len = Config.get('windowing', 'cw_len')
+    options.cw_shift = Config.get('windowing', 'cw_shift')
+
+    # [cnn]
+    options.cnn_N_filt = Config.get('cnn', 'cnn_N_filt')
+    options.cnn_len_filt = Config.get('cnn', 'cnn_len_filt')
+    options.cnn_max_pool_len = Config.get('cnn', 'cnn_max_pool_len')
+    options.cnn_use_laynorm_inp = Config.get('cnn', 'cnn_use_laynorm_inp')
+    options.cnn_use_batchnorm_inp = Config.get('cnn', 'cnn_use_batchnorm_inp')
+    options.cnn_use_laynorm = Config.get('cnn', 'cnn_use_laynorm')
+    options.cnn_use_batchnorm = Config.get('cnn', 'cnn_use_batchnorm')
+    options.cnn_act = Config.get('cnn', 'cnn_act')
+    options.cnn_drop = Config.get('cnn', 'cnn_drop')
+
+    # [dnn]
+    options.fc_lay = Config.get('dnn', 'fc_lay')
+    options.fc_drop = Config.get('dnn', 'fc_drop')
+    options.fc_use_laynorm_inp = Config.get('dnn', 'fc_use_laynorm_inp')
+    options.fc_use_batchnorm_inp = Config.get('dnn', 'fc_use_batchnorm_inp')
+    options.fc_use_batchnorm = Config.get('dnn', 'fc_use_batchnorm')
+    options.fc_use_laynorm = Config.get('dnn', 'fc_use_laynorm')
+    options.fc_act = Config.get('dnn', 'fc_act')
+
+    # [class]
+    options.class_lay = Config.get('class', 'class_lay')
+    options.class_drop = Config.get('class', 'class_drop')
+    options.class_use_laynorm_inp = Config.get('class', 'class_use_laynorm_inp')
+    options.class_use_batchnorm_inp = Config.get('class', 'class_use_batchnorm_inp')
+    options.class_use_batchnorm = Config.get('class', 'class_use_batchnorm')
+    options.class_use_laynorm = Config.get('class', 'class_use_laynorm')
+    options.class_act = Config.get('class', 'class_act')
+
+    # [optimization]
+    options.lr = Config.get('optimization', 'lr')
+    options.batch_size = Config.get('optimization', 'batch_size')
+    options.N_epochs = Config.get('optimization', 'N_epochs')
+    options.N_batches = Config.get('optimization', 'N_batches')
+    options.N_dev_batches = Config.get('optimization', 'N_dev_batches')
+    options.N_eval_epoch = Config.get('optimization', 'N_eval_epoch')
+    options.seed = Config.get('optimization', 'seed')
+
+    return options
+
+
+def str_to_bool(s):
+    if s == 'True':
+        return True
+    elif s == 'False':
+        return False
+    else:
+        raise ValueError
---
+++ WaveletCNN/Wavelet/train.py
@@ -0,0 +1,131 @@
+import os
+import soundfile as sf
+from conf import *
+from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, CSVLogger
+import numpy as np
+from keras.utils import to_categorical
+from keras.optimizers import RMSprop
+from keras import backend as K
+import librosa as lb
+
+K.clear_session()
+
+
+def batchGenerator(batch_size, data_folder, wav_lst, N_snt, wlen, fact_amp, out_dim):
+    while True:
+        sig_batch, lab_batch = create_batches_rnd(batch_size, data_folder, wav_lst, N_snt, wlen, fact_amp,
+                                                  out_dim)
+        yield sig_batch, lab_batch
+
+
+def create_batches_rnd(batch_size, data_folder, flac_lst, N_snt, wlen, fact_amp, out_dim):
+    # Initialization of the minibatch (batch_size,[0=>x_t,1=>x_t+N,1=>random_samp])
+    sig_batch = []
+    lab_batch = []
+    snt_id_arr = np.random.randint(N_snt, size=batch_size)
+    rand_amp_arr = np.random.uniform(1.0 - fact_amp, 1 + fact_amp, batch_size)
+
+    for i in range(int(batch_size / 2)):
+        # select a random sentence from the list
+        # [fs,signal]=scipy.io.wavfile.read(data_folder+wav_lst[snt_id_arr[i]])
+        # signal=signal.astype(float)/32768
+        file_id = flac_lst.iloc[snt_id_arr[i],1]
+        [signal, fs] = sf.read(data_folder+file_id+'.flac')
+        # accesing to a random chunk
+        snt_len = signal.shape[0]
+        snt_beg = np.random.randint(snt_len - wlen - 1)  # randint(0, snt_len-2*wlen-1)
+        snt_end = snt_beg + wlen
+        sig_batch.append(signal[snt_beg:snt_end] * rand_amp_arr[i])
+        y = flac_lst.iloc[snt_id_arr[i], -1]
+        lab_batch.append(y)
+        speaker_id = flac_lst.iloc[snt_id_arr[i], 0]
+        if flac_lst.iloc[snt_id_arr[i], -1] == 0:
+            selected_labels = flac_lst.loc[(flac_lst.iloc[:, -1] == 1) & (flac_lst.iloc[:, 0] == speaker_id)]
+        elif flac_lst.iloc[snt_id_arr[i], -1] == 1:
+            selected_labels = flac_lst.loc[(flac_lst.iloc[:, -1] == 0) & (flac_lst.iloc[:, 0] == speaker_id)]
+        label = selected_labels.sample()
+        index = flac_lst.loc[(flac_lst.iloc[:, 1] == label.values[0][1])].index
+        file_id = flac_lst.iloc[index,1].values[0]
+        [signal, fs] = sf.read(str(data_folder+file_id+'.flac'))
+        signal_len = signal.shape[0]
+        signal_beg = np.random.randint(signal_len - wlen - 1)  # randint(0, snt_len-2*wlen-1)
+        signal_end = signal_beg + wlen
+        sig_batch.append(signal[signal_beg:signal_end] * rand_amp_arr[i])
+        y = flac_lst.iloc[index, -1].values[0]
+        lab_batch.append(y)
+    sig_batch = np.array(sig_batch)
+    lab_batch = np.array(lab_batch)
+
+    idx = np.random.permutation(len(sig_batch))
+    x, y = sig_batch[idx], lab_batch[idx]
+    a, b = np.shape(x)
+    sig_batch = x.reshape((a, b, 1))
+    #     print(sig_batch.shape)
+    return sig_batch, to_categorical(np.array(y), num_classes=out_dim)
+
+
+def batchGenerator_val(batch_size, data_folder, wav_lst, N_snt, wlen, fact_amp, out_dim):
+    while True:
+        sig_batch, lab_batch = create_batches_rnd_val(batch_size, data_folder, wav_lst, N_snt, wlen, fact_amp,
+                                                  out_dim)
+        yield sig_batch, lab_batch
+
+
+def create_batches_rnd_val(batch_size,data_folder,flac_lst, N_snt, wlen,fact_amp, out_dim):
+    # Initialization of the minibatch (batch_size,[0=>x_t,1=>x_t+N,1=>random_samp])
+    sig_batch=np.zeros([batch_size,wlen])
+    lab_batch=[]
+    snt_id_arr=np.random.randint(N_snt, size=batch_size)
+    rand_amp_arr = np.random.uniform(1.0-fact_amp,1+fact_amp,batch_size)
+    for i in range(batch_size):
+        # select a random sentence from the list
+        #[fs,signal]=scipy.io.wavfile.read(data_folder+wav_lst[snt_id_arr[i]])
+        #signal=signal.astype(float)/32768
+        file_id = flac_lst.iloc[snt_id_arr[i],1]
+        [signal, fs] = sf.read(str(data_folder+file_id+'.flac'))
+        # accesing to a random chunk
+        snt_len=signal.shape[0]
+        snt_beg=np.random.randint(snt_len-wlen-1) #randint(0, snt_len-2*wlen-1)
+        snt_end=snt_beg+wlen
+        sig_batch[i,:]=signal[snt_beg:snt_end]*rand_amp_arr[i]
+        y= flac_lst.iloc[snt_id_arr[i],-1]
+        yt = to_categorical(y, num_classes=out_dim)
+        lab_batch.append(yt)
+    a, b = np.shape(sig_batch)
+    sig_batch = sig_batch.reshape((a, b, 1))
+    return sig_batch, np.array(lab_batch)
+
+print('N_filt ' + str(cnn_N_filt))
+print('N_filt len ' + str(cnn_len_filt))
+print('FS ' + str(fs))
+print('WLEN ' + str(wlen))
+
+input_shape = (wlen, 1)
+out_dim = class_lay[0]
+from model import getModel
+
+model = getModel(input_shape, out_dim)
+optimizer = RMSprop(lr=[0], rho=0.9, epsilon=1e-8)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+checkpoints_path = os.path.join(output_folder, 'checkpoints')
+
+tb = TensorBoard(log_dir=os.path.join(output_folder, 'logs', 'SincNet'))
+checkpointer = ModelCheckpoint(
+    filepath=os.path.join(checkpoints_path, 'SincNet.hdf5'),
+    verbose=1,
+    save_best_only=False)
+
+if not os.path.exists(checkpoints_path):
+    os.makedirs(checkpoints_path)
+
+
+callbacks = [tb, checkpointer]
+
+if pt_file != 'none':
+    model.load_weights(pt_file)
+
+train_generator = batchGenerator(batch_size, train_data_folder, flac_lst_train, snt_train, wlen, 0.2, out_dim)
+validation_generator = batchGenerator_val(Batch_dev, dev_data_folder, flac_lst_dev, snt_dev, wlen, 0.2, out_dim)
+model.fit_generator(train_generator, steps_per_epoch=N_batches, epochs=N_epochs, verbose=1,
+                    validation_data=validation_generator, validation_steps=N_dev_batches, callbacks=callbacks)
